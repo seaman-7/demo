d575ad90e6fdcf8efccf24660f8a528feca4c01c
seaman-7 <1299332802@qq.com>
2016-10-14 11:32:26 +0800

    增添刷新按钮

--- Light/src/components/Message.vue
+++ Light/src/components/Message.vue
@@ -5,7 +5,8 @@
     <p>评论、吐槽都在这，欢迎盖楼</p>
   </div>
   <!-- <span class="fresh" @click="myrefresh()">&加载不成功，点我试试...&</span> -->
-  <p id="c"><a @click="reload()" class="button button-light">点我刷新</a></p>
+  <span @click="reload()" class="icon icon-refresh"></span>
+  <!-- <p id="c"><a @click="reload()" class="button button-light">点我刷新</a></p> -->
 	<div class="ds-thread" data-thread-key="message" data-title="vue_jd&message" data-url="http://seaman-7.github.io/message"></div>
 	</div>
 </template>
@@ -13,6 +14,17 @@
 
 <style>
 /*<!-- <style scoped> -->*/
+.icon-refresh{
+	position: absolute;
+	bottom: 20px;
+	right: 20px;
+}
+.icon-refresh:hover{
+	transition: 1s;
+	-moz-transform:rotate(720deg);
+	-webkit-transform:rotate(720deg);
+	filter:progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
+}
 .message{
 	text-align:center;
 	background: url("http://ww4.sinaimg.cn/large/0060lm7Tgw1f8rlzbboigj30zk0k0dgq.jpg");
